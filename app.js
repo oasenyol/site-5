87dc4efc8cfb7b1321a6808ca480a3ec4a87bc17
Onur Ahmet Şenyol <oasenyol@gmail.com>
2022-01-15 04:33:25 +0300

    accordion menu tamamlandi

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ for(let i=0;i<tabHeaderNodes.length;i++){
     tabIndicator.style.left = `calc(calc(calc(25% - 5px) * ${i}) + 10px)`;
   });
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    
+    this.classList.toggle("active");
+
+    
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
